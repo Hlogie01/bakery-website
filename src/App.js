b0be6e5af7c2168195e01d739c350af663b36927
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-11 19:36:22 +0200

    app.js update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import AboutPage from './AboutPage';
 import MenuPage from './MenuPage';
 import ContactPage from './ContactPage';
 import Navigation from './Navigation';
+import Footer from './Footer';
 import './styles.css';
 
 const App = () => (
   <Router>
     <div>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <div style={{ minHeight: '80vh' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   </Router>
 );
