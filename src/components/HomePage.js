a7f13c482c5538ae68042baed915a59ed29de32d
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-13 00:34:24 +0200

    home update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import welcomeVideo from '../assets/videos/bakery.mp4';
+import pastryImage from '../assets/images/Almond & Cherry croissants.jpg';
 
 const HomePage = () => (
   <div>
     <h1>Welcome to Hlogi's Confectioneries</h1>
-    <video controls>
-      <source src="path_to_your_welcome_video.mp4" type="video/mp4" />
+    <video controls autoplay width="100%">
+      <source src={welcomeVideo} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     <p>Step into Hlogi’s Confectioneries, where we delight in crafting an array of delectable pastries using the finest ingredients and a generous sprinkle of love.</p>
     <div className="pastry-of-the-week">
-      <img src="path_to_past.jpg" alt="Pastry of the Week" />
+      <img src={pastryImage} alt="Pastry of the Week" />
       <p>Introducing our Pastry of the Week—a tantalizing delight you won’t want to overlook!</p>
     </div>
     <p>Discover our menu by clicking the button below:</p>
