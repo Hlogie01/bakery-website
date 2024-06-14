5972f12df1d18d590d3a92baecad2a585f357ced
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-14 21:34:51 +0200

    home page css updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,19 +5,23 @@ import pastryImage from '../assets/images/Almond & Cherry croissants.jpg';
 const HomePage = () => (
   <div>
     <h1>Welcome to Hlogi's Confectioneries</h1>
-    <video controls autoplay width="100%">
-      <source src={welcomeVideo} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+    <div className="video-menu-section">
+      <video width="300" controls autoPlay loop muted>
+        <source src={welcomeVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="menu-info">
+        <p>Explore our delicious pastries here:</p>
+        <button>
+          <a href="/menu">Our Menu</a>
+        </button>
+      </div>
+    </div>
     <p>Step into Hlogi’s Confectioneries, where we delight in crafting an array of delectable pastries using the finest ingredients and a generous sprinkle of love.</p>
     <div className="pastry-of-the-week">
       <img src={pastryImage} alt="Pastry of the Week" />
       <p>Introducing our Pastry of the Week—a tantalizing delight you won’t want to overlook!</p>
     </div>
-    <p>Discover our menu by clicking the button below:</p>
-    <button>
-      <a href="/menu">Our Menu</a>
-    </button>
   </div>
 );
 
